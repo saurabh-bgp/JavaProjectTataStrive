234ea68ed4f1e0e0b0220d6bdf83bcbb1963bdb5
Saurabh Kumar <saurabh030198@gmail.com>
2024-08-05 11:24:11 +0530

    While loop using

--- src/LinkedListExample.java
+++ src/LinkedListExample.java
@@ -11,17 +11,28 @@ public class LinkedListExample {
         Node b = new Node(2);
         Node c = new Node(3);
         Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node g = new Node(7);
         a.node = b;
         b.node = c;
         c.node =d;
-        System.out.println(a);
-        System.out.println(a.node);
-        System.out.println(a.val);
-        System.out.println(a.node);
-        System.out.println(b.node);
-        System.out.println(c);
-        System.out.println(a.node.node.val);
-        System.out.println(d.node);
+        d.node = e;
+        e.node = f;
+        f.node = g;
+//        System.out.println(a);
+//        System.out.println(a.node);
+//        System.out.println(a.val);
+//        System.out.println(a.node);
+//        System.out.println(b.node);
+//        System.out.println(c);
+//        System.out.println(a.node.node.val);
+//        System.out.println(d.node);
+        Node temp = a;
+       while (temp!=null){
+           System.out.println(temp.val);
+           temp=temp.node;
+       }
 
     }
 }
